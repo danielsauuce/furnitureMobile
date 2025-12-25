87de814a2396df3527178643689276ec976ae1ae
Daniel <danieladeyd77@gmail.com>
2025-12-25 02:42:40 +0000

    Mobile: installed /mobile for frontend of the task management

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-blue-100">
-      <Text className="text-xl font-bold text-red-900">Welcome Furniture!</Text>
+      <Text className="text-3xl font-bold text-red-900">Olayinka Furniture Palace</Text>
     </View>
   );
 }
